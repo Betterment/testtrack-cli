4f82fcee194fb500c5d87d6d3026dd67e1bed844
Reggie Gomez <reggie.gomez@betterment.com>
2019-12-20 14:24:14 -0500

    Return error and clean up description

--- cmds/generate_timestamp.go
+++ cmds/generate_timestamp.go
@@ -2,17 +2,15 @@ package cmds
 
 import (
 	"io/ioutil"
-	"log"
-	"os"
 	"time"
 
 	"github.com/spf13/cobra"
 )
 
 var generateTimestampDoc = `
-Write the current UTC timestamp to the file 'testtrack/build_timestamp.txt' in a
-TestTrack project. This timestamp can be passed as a param by the TestTrack
-client when calling the split registry endpoint from the TestTrack server.
+Write the current UTC timestamp to the file 'testtrack/build_timestamp.txt'.
+This timestamp is used by TestTrack clients to request the version of the split
+registry active at the given time.
 `
 
 func init() {
@@ -33,14 +31,5 @@ func generateTimestamp() error {
 	const buildTimestampPath = "testtrack/build_timestamp.txt"
 	timestamp := []byte(time.Now().UTC().Format("2006-01-02T15:04:05Z"))
 
-	err := ioutil.WriteFile(buildTimestampPath, timestamp, 0644)
-	if e, ok := err.(*os.PathError); ok {
-		if e.Path == buildTimestampPath {
-			log.Fatal("Testtrack Directory Not Found: Make sure you are in a TestTrack project")
-		}
-	} else if err != nil {
-		log.Fatal(err)
-	}
-
-	return nil
+	return ioutil.WriteFile(buildTimestampPath, timestamp, 0644)
 }
