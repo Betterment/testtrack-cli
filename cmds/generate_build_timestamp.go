ba968e2754992f95e14dec6a76d68264077d6967
Alex Burgel <aburgel@betterment.com>
2020-02-07 14:35:32 -0500

    Bump version to 1.1.0 (#52)

--- cmds/generate_build_timestamp.go
+++ cmds/generate_build_timestamp.go
@@ -7,27 +7,27 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var generateTimestampDoc = `
+var generateBuildTimestampDoc = `
 Write the current UTC timestamp to the file 'testtrack/build_timestamp'.
 This timestamp is used by TestTrack clients to request the version of the split
 registry active at the given time.
 `
 
 func init() {
-	rootCmd.AddCommand(generateTimestampCmd)
+	rootCmd.AddCommand(generateBuildTimestampCmd)
 }
 
-var generateTimestampCmd = &cobra.Command{
-	Use:   "generate_timestamp",
+var generateBuildTimestampCmd = &cobra.Command{
+	Use:   "generate_build_timestamp",
 	Short: "Write the current UTC timestamp to 'testtrack/build_timestamp'",
-	Long:  generateTimestampDoc,
+	Long:  generateBuildTimestampDoc,
 	Args:  cobra.NoArgs,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		return generateTimestamp()
+		return generateBuildTimestamp()
 	},
 }
 
-func generateTimestamp() error {
+func generateBuildTimestamp() error {
 	timestamp := []byte(time.Now().UTC().Format("2006-01-02T15:04:05Z"))
 
 	return ioutil.WriteFile("testtrack/build_timestamp", timestamp, 0644)
