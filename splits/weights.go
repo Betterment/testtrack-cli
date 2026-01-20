39f8ca872bb1f2e94eeae99b5e9f9e8d5185e221
Ray Zane <raymondzane@gmail.com>
2026-01-20 11:12:40 -0500

    Remove WeightsFromYAML and ToYAML

--- splits/weights.go
+++ splits/weights.go
@@ -2,9 +2,6 @@ package splits
 
 import (
 	"fmt"
-	"sort"
-
-	"gopkg.in/yaml.v2"
 )
 
 // Weights represents the weightings of a split
@@ -24,37 +21,6 @@ func NewWeights(weights map[string]int) (*Weights, error) {
 	}
 	w := Weights(weights)
 	return &w, nil
-}
-
-// WeightsFromYAML converts YAML-serializable weights to a weights map
-func WeightsFromYAML(yamlWeights yaml.MapSlice) (*Weights, error) {
-	weights := make(map[string]int)
-	for _, item := range yamlWeights {
-		variant, ok := item.Key.(string)
-		if !ok {
-			return nil, fmt.Errorf("variant %v is not a string", item.Key)
-		}
-		weight, ok := item.Value.(int)
-		if !ok {
-			return nil, fmt.Errorf("weighting %v is not an int", item.Value)
-		}
-		weights[variant] = weight
-	}
-	return NewWeights(weights)
-}
-
-// ToYAML converts weights to a YAML-serializable representation
-func (w *Weights) ToYAML() yaml.MapSlice {
-	var variants = make([]string, 0, len(*w))
-	for variant := range *w {
-		variants = append(variants, variant)
-	}
-	sort.Strings(variants)
-	weightsYaml := make(yaml.MapSlice, 0, len(variants))
-	for _, variant := range variants {
-		weightsYaml = append(weightsYaml, yaml.MapItem{Key: variant, Value: (*w)[variant]})
-	}
-	return weightsYaml
 }
 
 // Merge newWeights over weights
